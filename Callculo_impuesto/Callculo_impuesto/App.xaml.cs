3c33845233ba53295cfe91edf6124bf6668bc5de
Fabiana <pesantesfabiana454@gmail.com>
2026-06-14 23:54:42 -0500

    Calculo_impuestos

---
+++ Callculo_impuesto/Callculo_impuesto/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Callculo_impuesto
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

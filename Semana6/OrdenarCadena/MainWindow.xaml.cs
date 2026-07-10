82c12512cef15359c0458686cbad461e5010ac8e
Fabiana <pesantesfabiana454@gmail.com>
2026-07-10 08:32:25 -0500

    Código completo ordenar cadena

--- Semana6/OrdenarCadena/MainWindow.xaml.cs
+++ Semana6/OrdenarCadena/MainWindow.xaml.cs
@@ -16,9 +16,50 @@ namespace OrdenarCadena
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string [] ListaNombres;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btListar_Click(object sender, RoutedEventArgs e)
+        {
+            string nombres = txtNombres.Text;
+            if (string.IsNullOrEmpty(nombres))
+            {
+                MessageBox.Show("Por favor, ingrese nombres separados por espaios.", "Validación", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            ListaNombres = nombres.Split(' ');
+
+            //por cada nombre en la caja de texto se va a ir agregnadno uno por uso sin considerar espacios con split y se guarda en lso arreglos
+            foreach(string nombre in ListaNombres)
+            {
+                lbNombres.Items.Add(nombre);
+            }
+
+            txtTotalNombre.Text = lbNombres.Items.Count.ToString();
+        }
+
+        private void btPasar_Click(object sender, RoutedEventArgs e)
+        {
+            string letra = txtLetraNombreFiltrado.Text;
+
+            if (string.IsNullOrWhiteSpace(letra))
+            { 
+                MessageBox.Show("Ingresar la letra a filtrar", 
+                    "Validación", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            foreach(string nombre in ListaNombres)
+            {
+                if (nombre.StartsWith(letra,StringComparison.OrdinalIgnoreCase))
+                {
+                    lbNombresFiltrados.Items.Add(nombre);
+                }
+            }
+            txtTotalNombreFiltrados.Text = lbNombresFiltrados.Items.Count.ToString();
         }
     }
 }

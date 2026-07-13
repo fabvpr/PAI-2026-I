76a3e70263ac786cbe89ce642f3c858c99a49139
Fabiana <pesantesfabiana454@gmail.com>
2026-07-13 08:22:55 -0500

    Adivina Edad código Completo Implementado

--- Semana07/AdivinarEdad/MainWindow.xaml.cs
+++ Semana07/AdivinarEdad/MainWindow.xaml.cs
@@ -16,10 +16,60 @@ namespace AdivinarEdad
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int PrimerIntervalo;
+        private int SegundoIntervalo;
+        private int edadPropuesta;
+        private int contadorIntentos;
+        Random random = new Random();
         public MainWindow()
         {
             InitializeComponent();
         }
+        private void btIntento_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(txtPrimerIntervalo.Text, out PrimerIntervalo))
+            {
+                MessageBox.Show("El primer intervalo debe ser un número entero válido.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
 
+            if (!int.TryParse(txtSegundoIntervalo.Text, out SegundoIntervalo))
+            {
+                MessageBox.Show("El segundo intervalo debe ser un número entero válido.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (PrimerIntervalo >= SegundoIntervalo)
+            {
+                MessageBox.Show("El primer intervalo debe ser menor que el segundo intervalo.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            edadPropuesta = random.Next(PrimerIntervalo, SegundoIntervalo+1);
+            contadorIntentos++;
+            txtResultado.Text=edadPropuesta.ToString();
+        }
+
+        private void btCorrecto_Click(object sender, RoutedEventArgs e)
+        {
+            if (contadorIntentos == 0)
+            {
+                MessageBox.Show("Debe hacer click en el botón 'Intento'", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            MessageBox.Show($"El número de intentos fue: {contadorIntentos}");
+        }
+
+        private void btIncorrecto_Click(object sender, RoutedEventArgs e)
+        {
+            if (contadorIntentos == 0)
+            {
+                MessageBox.Show("Debe hacer click en el botón 'Intento'", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            edadPropuesta = random.Next(PrimerIntervalo, SegundoIntervalo + 1);
+            contadorIntentos++;
+            txtResultado.Text = edadPropuesta.ToString();
+        }
     }
 }
